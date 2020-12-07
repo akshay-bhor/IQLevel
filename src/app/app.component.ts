725cd04a41e0e25543599b0a8f964cb255adffa0
Akshay Bhor <akbhor50@gmail.com>
2020-12-07 18:37:21 +0530

    restore test Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent {
     if(chk == 0) {
       sideNav.close();
     }
-    else {s
+    else {
       //CHECK IF NEED TO CLOSE
       if(sideNav.open)
         sideNav.close();
